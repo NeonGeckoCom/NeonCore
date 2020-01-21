Jarbas-Core/mycroft-core

bus message targetting

mycroft/messagebus/message.py
@@ -76,16 +76,34 @@ class Message:
                        obj.get('data') or {},
                        obj.get('context') or {})
 
+    def forward(self, type, data=None):
+        """ Keep context and forward message
+
+        This will take the same parameters as a message object but use
+        the current message object as a reference.  It will copy the context
+        from the existing message object.
+
+        Args:
+            type (str): type of message
+            data (dict): data for message
+            context: intented context for new message
+
+        Returns:
+            Message: Message object to be used on the reply to the message
+        """
+        data = data or {}
+        return Message(type, data, context=self.context)
+
     def reply(self, type, data=None, context=None):
         """Construct a reply message for a given message
 
         This will take the same parameters as a message object but use
         the current message object as a reference.  It will copy the context
         from the existing message object and add any context passed in to
-        the function.  Check for a target passed in to the function from
-        the data object and add that to the context as a target.  If the
-        context has a client name then that will become the target in the
-        context.  The new message will then have data passed in plus the
+        the function.  Check for a destination passed in to the function from
+        the data object and add that to the context as a destination.  If the
+        context has a source then that will be swapped with the destination
+        in the context.  The new message will then have data passed in plus the
         new context generated.
 
         Args:
@@ -102,10 +120,12 @@ class Message:
         new_context = self.context
         for key in context:
             new_context[key] = context[key]
-        if 'target' in data:
-            new_context['target'] = data['target']
-        elif 'client_name' in context:
-            context['target'] = context['client_name']
+        if 'destination' in data:
+            new_context['destination'] = data['destination']
+        if 'source' in new_context and 'destination' in new_context:
+            s = new_context['destination']
+            new_context['destination'] = new_context['source']
+            new_context['source'] = s
         return Message(type, data, context=new_context)
 
     def response(self, data=None, context=None):

NeonGeckoCom/NeonCore

mycroft/messagebus/message.py
@@ -74,16 +74,33 @@ class Message:
                        obj.get('data') or {},
                        obj.get('context') or {})
 
+    def forward(self, msg_type, data=None):
+        """ Keep context and forward message
+
+        This will take the same parameters as a message object but use
+        the current message object as a reference.  It will copy the context
+        from the existing message object.
+
+        Args:
+            msg_type (str): type of message
+            data (dict): data for message
+
+        Returns:
+            Message: Message object to be used on the reply to the message
+        """
+        data = data or {}
+        return Message(msg_type, data, context=self.context)
+
     def reply(self, msg_type, data=None, context=None):
         """Construct a reply message for a given message
 
         This will take the same parameters as a message object but use
         the current message object as a reference.  It will copy the context
         from the existing message object and add any context passed in to
-        the function.  Check for a target passed in to the function from
-        the data object and add that to the context as a target.  If the
-        context has a client name then that will become the target in the
-        context.  The new message will then have data passed in plus the
+        the function.  Check for a destination passed in to the function from
+        the data object and add that to the context as a destination.  If the
+        context has a source then that will be swapped with the destination
+        in the context.  The new message will then have data passed in plus the
         new context generated.
 
         Args:
@@ -100,10 +117,12 @@ class Message:
         new_context = self.context
         for key in context:
             new_context[key] = context[key]
-        if 'target' in data:
-            new_context['target'] = data['target']
-        elif 'client_name' in context:
-            context['target'] = context['client_name']
+        if 'destination' in data:
+            new_context['destination'] = data['destination']
+        if 'source' in new_context and 'destination' in new_context:
+            s = new_context['destination']
+            new_context['destination'] = new_context['source']
+            new_context['source'] = s
         return Message(msg_type, data, context=new_context)
 
     def response(self, data=None, context=None):