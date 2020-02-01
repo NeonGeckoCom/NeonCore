2b4b5cdcffbe0b9a5e7996493b242fab95431aa6
jarbasal <jarbasai@mailfence.com>
2020-02-01 21:43:06 +0000

    hive mind integration

--- mycroft/hivemind/__main__.py
+++ mycroft/hivemind/__main__.py
@@ -4,6 +4,8 @@ from mycroft.messagebus.client import MessageBusClient
 from mycroft.util import create_daemon
 
 config = Configuration.get().get("hivemind")
+
 bus = MessageBusClient()
 create_daemon(bus.run_forever)
+
 start_mind(config, bus)
