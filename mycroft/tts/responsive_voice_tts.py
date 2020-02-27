d76d3e2c52b77cca8bf5309a6b10fb1dc73e4b27
jarbasal <jarbasai@mailfence.com>
2020-02-27 05:27:09 +0000

    TTS/responsive_voice

--- mycroft/tts/responsive_voice_tts.py
+++ mycroft/tts/responsive_voice_tts.py
@@ -14,38 +14,59 @@
 #
 import requests
 from mycroft.tts import TTS, TTSValidator
+from mycroft.util.log import LOG
 
 
-class ResponsiveVoice(TTS):
+class ResponsiveVoiceTTS(TTS):
     def __init__(self, lang, config):
-        super(ResponsiveVoice, self).__init__(
+        super().__init__(
             lang, config, ResponsiveVoiceValidator(self), 'mp3',
             ssml_tags=[]
         )
-        self.clear_cache()
+        from responsive_voice import get_voice, ResponsiveVoice
+
         self.pitch = config.get("pitch", 0.5)
         self.rate = config.get("rate", 0.5)
         self.vol = config.get("vol", 1)
-        if "f" not in config.get("gender", "male"):
-            self.sv = "g1"
-            self.vn = "rjs"
+        if "f" in config.get("gender", ""):
+            self.gender = ResponsiveVoice.FEMALE
+        if "m" in config.get("gender", ""):
+            self.gender = ResponsiveVoice.MALE
         else:
-            self.vn = self.sv = ""
+            self.gender = ResponsiveVoice.UNKNOWN_GENDER
+        self.voice = config.get("voice")
+        if not self.voice:
+            self.engine = ResponsiveVoice(lang=self.lang,
+                                          pitch=self.pitch,
+                                          rate=self.rate,
+                                          vol=self.vol,
+                                          gender=self.gender)
+        else:
+            Voice = get_voice(self.voice, self.lang)
+            if not Voice:
+                # dont filter by language
+                Voice = get_voice(self.voice)
+            if not Voice:
+                raise
+            self.engine = Voice(pitch=self.pitch,
+                                rate=self.rate,
+                                vol=self.vol)
 
     def get_tts(self, sentence, wav_file):
-        params = {"t": sentence, "tl": self.lang,
-                  "pitch": self.pitch, "rate": self.rate,
-                  "vol": self.vol, "sv": self.sv, "vn": self.vn}
-        base_url = "http://responsivevoice.org/responsivevoice/getvoice.php"
-        r = requests.get(base_url, params)
-        with open(wav_file, "w") as f:
-            f.write(r.content)
+        self.engine.get_mp3(sentence, wav_file)
         return wav_file, None
 
 
 class ResponsiveVoiceValidator(TTSValidator):
     def __init__(self, tts):
         super(ResponsiveVoiceValidator, self).__init__(tts)
+
+    def validate_dependencies(self):
+        try:
+            import responsive_voice
+        except:
+            LOG.error("Run pip install ResponsiveVoice")
+            raise
 
     def validate_lang(self):
         # TODO: Verify responsive voice can handle the requested language
@@ -58,4 +79,4 @@ class ResponsiveVoiceValidator(TTSValidator):
         raise AssertionError("Could not reach http://responsivevoice.org")
 
     def get_tts_class(self):
-        return ResponsiveVoice
+        return ResponsiveVoiceTTS
