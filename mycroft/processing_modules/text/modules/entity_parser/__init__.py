6c2f780521d69a67b216105cf12140608e0a62bf
jarbasal <jarbasai@mailfence.com>
2020-12-16 18:29:24 +0000

    Rake keyword extractor

--- mycroft/processing_modules/text/modules/entity_parser/__init__.py
+++ mycroft/processing_modules/text/modules/entity_parser/__init__.py
@@ -3,24 +3,20 @@
 # might be useful to tag entities in the future
 # check https://github.com/OpenJarbas/simple_NER
 
-from simple_NER.annotators.keyword_ner import KeywordNER
+from RAKEkeywords import Rake
 from mycroft.processing_modules.text import TextParser
 
 
 class EntityTagger(TextParser):
     def __init__(self, name="keyword_tagger", priority=99):
         super().__init__(name, priority)
-        self.rake = KeywordNER()
 
     def parse(self, utterances, lang="en-us"):
         keywords = []
         for utterance in utterances:
             # extract keywords
-            ents = list(self.rake.extract_entities(utterance))  # generator, needs list()
-            # group into tuples of (keyword, score)
-            keywords += [(ent.value, ent.score) for ent in ents]
-        # sort by score
-        keywords = sorted(keywords, key=lambda kw: kw[1], reverse=True)
+            rake = Rake(lang)
+            keywords += rake.extract_keywords(utterance)
 
         # return unchanged utterances + data
         return utterances, {"keywords": keywords}
