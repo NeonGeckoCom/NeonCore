ac07cc716f36ed380a548409fd1edad9b41c06fb
jarbasal <jarbasai@mailfence.com>
2021-05-12 12:28:52 +0100

    feat/lang_plugins

--- mycroft/language/__init__.py
+++ mycroft/language/__init__.py
@@ -27,7 +27,6 @@ from ovos_plugin_manager.language import load_lang_detect_plugin, \
     load_tx_plugin
 from mycroft.util.log import LOG
 import os
-import boto3
 
 
 def langcode2name(lang_code):
@@ -573,57 +572,8 @@ class LangDetectDetector(LanguageDetector):
         return langs
 
 
-class AmazonTranslator(LanguageTranslator):
-    def __init__(self):
-        super().__init__()
-        self.keys = get_private_keys()["amazon"]
-        self.client = boto3.Session(aws_access_key_id=self.keys["key_id"],
-                                    aws_secret_access_key=self.keys[
-                                        "secret_key"],
-                                    region_name=self.keys["region"]).client(
-            'translate')
-
-    def translate(self, text, target=None, source="auto"):
-        target = target or self.internal_language
-
-        response = self.client.translate_text(
-            Text=text,
-            SourceLanguageCode="auto",
-            TargetLanguageCode=target.split("-")[0]
-        )
-        return response["TranslatedText"]
-
-
-class AmazonDetector(LanguageDetector):
-    def __init__(self):
-        super().__init__()
-        self.keys = get_private_keys()["amazon"]
-        self.client = boto3.Session(aws_access_key_id=self.keys["key_id"],
-                                    aws_secret_access_key=self.keys[
-                                        "secret_key"],
-                                    region_name=self.keys["region"]).client(
-            'comprehend')
-
-    def detect(self, text):
-        response = self.client.detect_dominant_language(
-            Text=text
-        )
-        return response['Languages'][0]['LanguageCode']
-
-    def detect_probs(self, text):
-        response = self.client.detect_dominant_language(
-            Text=text
-        )
-        langs = {}
-        for lang in response["Languages"]:
-            langs[lang["LanguageCode"]] = lang["Score"]
-        return langs
-
-
 class TranslatorFactory:
-    CLASSES = {
-        "amazon": AmazonTranslator
-    }
+    CLASSES = {}
 
     @staticmethod
     def create(module=None):
@@ -638,12 +588,12 @@ class TranslatorFactory:
             # old style, TODO deprecate once everything is a plugin
             return clazz()
         except:
+            config["keys"] = get_private_keys()
             return clazz(config)
 
 
 class DetectorFactory:
     CLASSES = {
-        "amazon": AmazonDetector,
         "cld2": Pycld2Detector,
         "cld3": Pycld3Detector,
         "fastlang": FastLangDetector,
@@ -664,4 +614,5 @@ class DetectorFactory:
             # old style, TODO deprecate once everything is a plugin
             return clazz()
         except:
+            config["keys"] = get_private_keys()
             return clazz(config)
