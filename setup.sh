bfd0488d50feb30a8623c0c6dc603ab252011acd
Daniel McKnight <daniel@neongecko.com>
2021-05-28 13:48:24 -0700

    Fix minor typos in setup script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 sudo apt install python3-dev python3-venv swig libssl-dev libfann-dev portaudio19-dev git
 
 git clone https://github.com/mycroftai/mycroft-gui
-bash mycroft-gui/setup.sh
-export GITHUB_TOKEN="{$1}"
+bash mycroft-gui/dev_setup.sh
+export GITHUB_TOKEN="${1}"
 
 pip install "git+https://${GITHUB_TOKEN}@github.com/NeonDaniel/NeonCore#egg=neon_core[dev,client]"
