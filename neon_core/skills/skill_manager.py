608505f755cb12b8b0942372a93fd5f617c514f0
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2021-06-01 11:02:38 -0700

    Handle Neon Skill Config (#87)
    * Update skills to use Neon config methods
    
    * Log error on remote auth failure
    
    * Override _get_skill_directories to use config skill directory
    
    * Method annotation and cleanup
    
    * Annotate future further integration with OSM

--- neon_core/skills/skill_manager.py
+++ neon_core/skills/skill_manager.py
@@ -1,7 +1,11 @@
+import os
+from glob import glob
+
 from mycroft.util.log import LOG
 from mycroft.util import connected
 from mycroft.skills.skill_manager import SkillManager
 from neon_core.skills.skill_store import SkillsStore
+from neon_utils.configuration_utils import get_neon_skills_config
 
 SKILL_MAIN_MODULE = '__init__.py'
 
@@ -10,14 +14,15 @@ class NeonSkillManager(SkillManager):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.skill_downloader = SkillsStore(skills_dir=self.msm.skills_dir,
+        self.skill_config = kwargs.get("config") or get_neon_skills_config()
+        self.skill_downloader = SkillsStore(skills_dir=self.skill_config["directory"], config=self.skill_config,
                                             bus=self.bus)
-        self.skill_downloader.skills_dir = self.msm.skills_dir
+        self.skill_downloader.skills_dir = self.skill_config["directory"]
 
     def download_or_update_defaults(self):
         # on launch only install if missing, updates handled separately
         # if osm is disabled in .conf this does nothing
-        if self.config["skills"]["auto_update"]:
+        if self.skill_config["auto_update"]:
             try:
                 self.skill_downloader.install_default_skills()
             except Exception as e:
@@ -41,3 +46,24 @@ class NeonSkillManager(SkillManager):
         reply = message.reply('skill.converse.error',
                               data=dict(skill_id=skill_id, error=error_msg))
         self.bus.emit(reply)
+
+    def _get_skill_directories(self):
+        """
+        Locates all skill directories in the configured skill install path
+        """
+        # TODO: Integrate this with OSM local appstores DM
+        base_skill_dir = glob(os.path.join(self.skill_config["directory"], "*/"))
+        skill_directories = []
+        for skill_dir in base_skill_dir:
+            # TODO: all python packages must have __init__.py!  Better way?
+            # check if folder is a skill (must have __init__.py)
+            if SKILL_MAIN_MODULE in os.listdir(skill_dir):
+                skill_directories.append(skill_dir.rstrip('/'))
+                if skill_dir in self.empty_skill_dirs:
+                    self.empty_skill_dirs.discard(skill_dir)
+            else:
+                if skill_dir not in self.empty_skill_dirs:
+                    self.empty_skill_dirs.add(skill_dir)
+                    LOG.debug('Found skills directory with no skill: ' +
+                              skill_dir)
+        return skill_directories
