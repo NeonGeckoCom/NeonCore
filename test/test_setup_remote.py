813aa3844480a7655e6ea4bd5fa58103c7c820cd
Daniel McKnight <daniel@neongecko.com>
2021-06-09 20:48:20 -0700

    Update dev_local testing Add remote testing

--- test/test_setup_remote.py
+++ test/test_setup_remote.py
@@ -2,21 +2,21 @@ import unittest
 from neon_utils.configuration_utils import get_neon_local_config
 
 
-class TestSetupDevLocal(unittest.TestCase):
+class TestSetupRemote(unittest.TestCase):
     def test_config_from_setup(self):
         local_config = get_neon_local_config()
-        self.assertTrue(local_config["prefFlags"]["devMode"])
-        self.assertEqual(local_config["stt"]["module"], "deepspeech_stream_local")
-        self.assertEqual(local_config["tts"]["module"], "mozilla_remote")
+        self.assertFalse(local_config["prefFlags"]["devMode"])
+        self.assertEqual(local_config["stt"]["module"], "google_cloud_streaming")
+        self.assertEqual(local_config["tts"]["module"], "polly")
         self.assertIsInstance(local_config["skills"]["neon_token"], str)
 
     def test_installed_packages(self):
-        import neon_tts_plugin_mozilla_remote
-        import neon_stt_plugin_deepspeech_stream_local
+        import neon_tts_plugin_polly
+        import neon_stt_plugin_google_cloud_streaming
         import mycroft
-        import neon_test_utils
         import neon_core_client
         with self.assertRaises(ImportError):
+            import neon_test_utils
             import neon_core_server
 
 
