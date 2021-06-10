8203ff9844ac78a771615f70244ed7e8a35c7e74
Daniel McKnight <daniel@neongecko.com>
2021-06-10 09:54:09 -0700

    Troubleshoot mimic setup test case

--- test/test_setup_local_mimic.py
+++ test/test_setup_local_mimic.py
@@ -2,10 +2,10 @@ import unittest
 from neon_utils.configuration_utils import get_neon_local_config
 
 
-class TestSetupDevLocal(unittest.TestCase):
+class TestSetupLocalMimic(unittest.TestCase):
     def test_config_from_setup(self):
         local_config = get_neon_local_config()
-        self.assertTrue(local_config["prefFlags"]["devMode"])
+        self.assertFalse(local_config["prefFlags"]["devMode"])
         self.assertEqual(local_config["stt"]["module"], "deepspeech_stream_local")
         self.assertEqual(local_config["tts"]["module"], "mimic")
         self.assertIsInstance(local_config["skills"]["neon_token"], str)
@@ -14,7 +14,6 @@ class TestSetupDevLocal(unittest.TestCase):
         import neon_tts_plugin_mozilla_remote
         import neon_stt_plugin_deepspeech_stream_local
         import mycroft
-        import neon_cli
         import neon_core_client
         with self.assertRaises(ImportError):
             import neon_core_server
