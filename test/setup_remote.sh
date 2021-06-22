6d71f53f5885fc96429f9bfec2cacfcbcc9ca494
Daniel McKnight <daniel@neongecko.com>
2021-06-21 18:18:27 -0700

    Fix typo in setup for added requirements

--- test/setup_remote.sh
+++ test/setup_remote.sh
@@ -62,7 +62,7 @@ if [ "${devMode}" == "true" ]; then
 fi
 optStr=$(printf ",%s" "${options[@]}")
 optStr="[${optStr:1}]"
-pipStr="git+https://${GITHUB_TOKEN}@github.com/NeonGeckoCom/NeonCore/tree/FEAT_UnitTestsSetup#egg=neon_core${optStr}"
+pipStr="git+https://${GITHUB_TOKEN}@github.com/NeonGeckoCom/NeonCore@FEAT_UnitTestsSetup#egg=neon_core${optStr}"
 
 # Create install directory if specified and doesn't exist
 if [ ! -d "${installerDir}" ]; then
