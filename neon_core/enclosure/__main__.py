17b083d8c56f0072858f86cc838bddad791c8a6d
Daniel McKnight <daniel@neongecko.com>
2021-08-06 01:22:06 -0700

    Idle Screen initial implementation

--- neon_core/enclosure/__main__.py
+++ neon_core/enclosure/__main__.py
@@ -23,10 +23,12 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 from neon_core.enclosure.gui import GUIManager
+from neon_core.enclosure.resting_screen import init_resting_screen_manager
 
 
 def main():
     gui = GUIManager()
+    init_resting_screen_manager()
     gui.run()
 
 
