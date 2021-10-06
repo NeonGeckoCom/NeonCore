9964981bdb7fd67a21c9c796722ec0e00255dde7
jarbasal <jarbasai@mailfence.com>
2021-10-06 15:05:52 +0100

    holmesV config

--- neon_core/configuration/__init__.py
+++ neon_core/configuration/__init__.py
@@ -1,10 +1,7 @@
-from neon_core.configuration.config import Configuration, LocalConf
-from neon_core.configuration.locations import *
-from ovos_utils.configuration import set_config_name
-
+from mycroft.configuration.config import Configuration, LocalConf
 
 def get_private_keys():
     return Configuration.get(remote=False).get("keys", {})
 
-# make ovos_utils load the proper .conf files
-set_config_name("neon", "neon_core")
+
+
