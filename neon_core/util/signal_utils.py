0862ea39b8429fe1d64df7ae2c30e5be29255052
Daniel McKnight <daniel@neongecko.com>
2021-12-06 22:30:21 -0800

    Add signal utils with unit tests

---
+++ neon_core/util/signal_utils.py
@@ -0,0 +1,247 @@
+# # NEON AI (TM) SOFTWARE, Software Development Kit & Application Development System
+# # All trademark and other rights reserved by their respective owners
+# # Copyright 2008-2021 Neongecko.com Inc.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS;  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from threading import Event
+from time import time
+from typing import Optional, Dict
+from mycroft_bus_client import MessageBusClient, Message
+from neon_utils.logger import LOG
+
+BUS = MessageBusClient()
+
+
+class Signal:
+    def __init__(self):
+        self.create_time = time()
+        self._created_event = Event()
+        self._cleared_event = Event()
+        self._created_event.clear()
+        self._cleared_event.set()
+
+    @property
+    def is_set(self):
+        """
+        Boolean state of signal creation
+        """
+        return self._created_event.is_set()
+
+    def create(self):
+        """
+        Marks this signal as created
+        """
+        self._cleared_event.clear()
+        self._created_event.set()
+        self.create_time = time()
+
+    def clear(self):
+        """
+        Marks this signal as cleared
+        """
+        self._created_event.clear()
+        self._cleared_event.set()
+
+    def wait_for_create(self, timeout: Optional[int] = None) -> bool:
+        """
+        Wait for the signal to be created
+        :param timeout: Seconds to wait for the signal to be set
+        :return: True if the signal is set
+        """
+        return self._created_event.wait(timeout)
+
+    def wait_for_clear(self, timeout: Optional[int] = None) -> bool:
+        """
+        Wait for the signal to be cleared
+        :param timeout: Seconds to wait for the signal to be cleared
+        :return: True if the signal is set
+        """
+        return not self._cleared_event.wait(timeout)
+
+
+class SignalManager:
+    def __init__(self, bus: MessageBusClient = None):
+        self._signals: Dict[str, Signal] = dict()
+        self.bus = bus or MessageBusClient()
+        self._register_listeners()
+        self.bus.run_in_thread()
+        if not self.bus.connected_event.wait(60):
+            LOG.error(f"Bus not connected after 60 seconds")
+
+    def create_signal(self, signal: str) -> bool:
+        """
+        Set the specified signal, creating it if it doesn't exist
+        """
+        self._ensure_signal_is_defined(signal)
+        self._signals[signal].create()
+        return True
+
+    def check_for_signal(self, signal: str, sec_lifetime: int = 0):
+        """
+        Check if the specified signal exists and is set, optionally clearing it
+        """
+        if signal not in self._signals or not self._signals[signal].is_set:
+            # Signal not defined or not set
+            return False
+        if sec_lifetime == 0:
+            # Clear the signal and return
+            self._signals[signal].clear()
+            return True
+        if sec_lifetime == -1:
+            # Return signal state (True)
+            return True
+        if self._signals[signal].create_time + sec_lifetime < time():
+            # Signal is expired and must be cleared
+            LOG.debug(f"Clearing expired signal: {signal}")
+            self._signals[signal].clear()
+            return False
+        # Signal exists and is not yet expired
+        return True
+
+    def wait_for_signal_set(self, signal: str, sec_timeout: Optional[int] = None) -> bool:
+        """
+        Wait for the specified signal to be set and return set state after timeout
+        """
+        self._ensure_signal_is_defined(signal)
+        return self._signals[signal].wait_for_create(sec_timeout)
+
+    def wait_for_signal_clear(self, signal: str, sec_timeout: Optional[int] = None) -> bool:
+        """
+        Wait for the specified signal to be set and return set state after timeout
+        """
+        self._ensure_signal_is_defined(signal)
+        return self._signals[signal].wait_for_clear(sec_timeout)
+
+    def _ensure_signal_is_defined(self, signal):
+        if signal not in self._signals or not isinstance(self._signals[signal], Signal):
+            self._signals[signal] = Signal()
+
+    def _register_listeners(self):
+        """
+        Register Event Handlers
+        """
+        self.bus.on("neon.create_signal", self._handle_create_signal)
+        self.bus.on("neon.check_for_signal", self._handle_check_for_signal)
+        self.bus.on("neon.wait_for_signal_create", self._handle_wait_for_signal_set)
+        self.bus.on("neon.wait_for_signal_clear", self._handle_wait_for_signal_clear)
+
+    def _handle_create_signal(self, message: Message):
+        signal_name = message.data["signal_name"]
+        status = self.create_signal(signal_name)
+        self.bus.emit(message.reply(f"signal_status.{signal_name}", data={"signal_name": signal_name,
+                                                                          "is_set": status}))
+
+    def _handle_check_for_signal(self, message: Message):
+        signal_name = message.data["signal_name"]
+        status = self.check_for_signal(signal_name, message.data.get("sec_lifetime", 0))
+        self.bus.emit(message.reply(f"signal_status.{signal_name}", data={"signal_name": signal_name,
+                                                                          "is_set": status}))
+
+    def _handle_wait_for_signal_set(self, message: Message):
+        signal_name = message.data["signal_name"]
+        status = self.wait_for_signal_set(signal_name, message.data.get("timeout"))
+        self.bus.emit(message.reply(f"signal_status.{signal_name}", data={"signal_name": signal_name,
+                                                                          "is_set": status}))
+
+    def _handle_wait_for_signal_clear(self, message: Message):
+        signal_name = message.data["signal_name"]
+        status = self.wait_for_signal_clear(signal_name, message.data.get("timeout"))
+        self.bus.emit(message.reply(f"signal_status.{signal_name}", data={"signal_name": signal_name,
+                                                                          "is_set": status}))
+
+
+def _ensure_service_is_alive(bus: MessageBusClient = BUS):
+    """
+    Method to ensure SignalManager service is alive
+    """
+    if not bus.started_running:
+        bus.run_in_thread()
+    if not bus.connected_event.wait(30):
+        raise RuntimeError("Bus unable to connect after 30 seconds!")
+    # TODO: Method to wait for signal service to come online DM
+
+
+def create_signal(signal_name: str, bus: MessageBusClient = BUS) -> bool:
+    """
+    Backwards-compatible method for creating a signal
+    :param signal_name: named signal to create
+    :param bus: MessagebusClient Object to connect with
+    :return: True if signal exists
+    """
+    _ensure_service_is_alive(bus)
+    stat = bus.wait_for_response(Message("neon.create_signal",
+                                         {"signal_name": signal_name}),
+                                 f"signal_status.{signal_name}")
+    return stat.data.get("is_set")
+
+
+def check_for_signal(signal_name: str, sec_lifetime: int = 0, bus: MessageBusClient = BUS) -> bool:
+    """
+    Backwards-compatible method for checking for a signal
+    :param signal_name: name of signal to check
+    :param sec_lifetime: max age of signal in seconds before clearing it and returning False
+    :param bus: MessagebusClient Object to connect with
+    :return: True if signal exists
+    """
+    _ensure_service_is_alive(bus)
+    stat = bus.wait_for_response(Message("neon.check_for_signal",
+                                         {"signal_name": signal_name,
+                                          "sec_lifetime": sec_lifetime}),
+                                 f"signal_status.{signal_name}", 10)
+    return stat.data.get("is_set")
+
+
+def wait_for_signal_create(signal_name: str, timeout: int = 30, bus: MessageBusClient = BUS) -> bool:
+    """
+    Block until the specified signal is set or timeout is reached
+    :param signal_name: name of signal to check
+    :param timeout: max seconds to wait for signal to be created, Default is 30 seconds
+    :param bus: MessagebusClient Object to connect with
+    :return: True if signal exists
+    """
+    timeout = 300 if timeout > 300 else timeout  # Cap wait at 5 minutes
+    bus_wait_time = timeout + 5  # Allow some padding for bus handler
+    _ensure_service_is_alive(bus)
+    stat = bus.wait_for_response(Message("neon.wait_for_signal_create",
+                                         {"signal_name": signal_name,
+                                          "timeout": timeout}),
+                                 f"signal_status.{signal_name}", bus_wait_time)
+    return stat.data.get("is_set")
+
+
+def wait_for_signal_clear(signal_name: str, timeout: int = 30, bus: MessageBusClient = BUS) -> bool:
+    """
+    Block until the specified signal is cleared or timeout is reached
+    :param signal_name: name of signal to check
+    :param timeout: max seconds to wait for signal to be created, Default is 30 seconds
+    :param bus: MessagebusClient Object to connect with
+    :return: True if signal exists
+    """
+    timeout = 300 if timeout > 300 else timeout  # Cap wait at 5 minutes
+    bus_wait_time = timeout + 5  # Allow some padding for bus handler
+    _ensure_service_is_alive(bus)
+    stat = bus.wait_for_response(Message("neon.wait_for_signal_clear",
+                                         {"signal_name": signal_name,
+                                          "timeout": timeout}),
+                                 f"signal_status.{signal_name}", bus_wait_time)
+    return stat.data.get("is_set")
---
+++ test/test_signal_utils.py
@@ -0,0 +1,93 @@
+# # NEON AI (TM) SOFTWARE, Software Development Kit & Application Development System
+# # All trademark and other rights reserved by their respective owners
+# # Copyright 2008-2021 Neongecko.com Inc.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS;  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import os
+import sys
+import unittest
+from multiprocessing import Process
+from threading import Thread
+from time import sleep
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from neon_core.util.signal_utils import *
+from neon_core.messagebus.service.__main__ import main as messagebus_service
+
+
+class TestSignalUtils(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.bus_thread = Process(target=messagebus_service, daemon=False)
+        cls.bus_thread.start()
+        cls.signal_manager = SignalManager()
+        sleep(15)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.bus_thread.terminate()
+
+    def test_create_signal(self):
+        self.assertTrue(create_signal("test_signal"))
+        self.assertTrue(create_signal("test_signal"))
+        self.assertIsInstance(self.signal_manager._signals["test_signal"], Signal)
+        self.assertTrue(self.signal_manager._signals["test_signal"].is_set)
+        self.signal_manager._signals.pop('test_signal')
+
+    def test_check_for_signal(self):
+        self.assertFalse(check_for_signal("test_signal"))
+        create_signal("test_signal")
+        self.assertTrue(check_for_signal("test_signal", -1))
+        self.assertTrue(check_for_signal("test_signal", 60))
+        self.assertTrue(check_for_signal("test_signal"))
+        self.assertFalse(check_for_signal("test_signal"))
+        create_signal("test_signal")
+        self.assertTrue(check_for_signal("test_signal", 15))
+        sleep(2)
+        self.assertTrue(check_for_signal("test_signal", 15))
+        self.assertFalse(check_for_signal("test_signal", 2))
+        self.assertFalse(check_for_signal("test_signal"))
+
+    def test_wait_for_signal_create(self):
+        def create_test_signal():
+            sleep(3)
+            create_signal("test_signal")
+        self.assertFalse(wait_for_signal_create("test_signal", 1))
+        Thread(target=create_test_signal).start()
+        self.assertTrue(wait_for_signal_create("test_signal", 5))
+        self.assertTrue(check_for_signal("test_signal"))
+
+    def test_wait_for_signal_clear(self):
+        def clear_test_signal():
+            sleep(3)
+            check_for_signal("test_signal")
+        self.assertFalse(wait_for_signal_clear("test_signal", 1))
+        create_signal("test_signal")
+        self.assertTrue(wait_for_signal_clear("test_signal", 1))
+        Thread(target=clear_test_signal).start()
+        self.assertFalse(wait_for_signal_clear("test_signal", 5))
+        self.assertFalse(check_for_signal("test_signal"))
+
+
+if __name__ == '__main__':
+    unittest.main()
