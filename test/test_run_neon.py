e7a0782cf100c2b5306cddc76f545b7f143a3c1e
Daniel McKnight <daniel@neongecko.com>
2022-01-12 15:46:58 -0800

    Troubleshooting local test failures

--- test/test_run_neon.py
+++ test/test_run_neon.py
@@ -52,7 +52,7 @@ class TestRunNeon(unittest.TestCase):
         cls.bus = MessageBusClient()
         cls.bus.run_in_thread()
         cls.bus.connected_event.wait()
-        cls.bus.wait_for_message("mycroft.ready", 450)
+        cls.bus.wait_for_message("mycroft.ready", 360)
 
     @classmethod
     def tearDownClass(cls) -> None:
