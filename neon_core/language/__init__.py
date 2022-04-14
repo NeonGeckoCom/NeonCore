179fef42afb29c3991766b5db11a8606b39bfbac
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2022-04-14 15:40:15 -0700

    Update license headers to updated 2022 version (#225)
    Co-authored-by: Daniel McKnight <daniel@neon.ai>

--- neon_core/language/__init__.py
+++ neon_core/language/__init__.py
@@ -1,6 +1,9 @@
-# # NEON AI (TM) SOFTWARE, Software Development Kit & Application Development System
-# # All trademark and other rights reserved by their respective owners
-# # Copyright 2008-2021 Neongecko.com Inc.
+# NEON AI (TM) SOFTWARE, Software Development Kit & Application Framework
+# All trademark and other rights reserved by their respective owners
+# Copyright 2008-2022 Neongecko.com Inc.
+# Contributors: Daniel McKnight, Guy Daniels, Elon Gasper, Richard Leeds,
+# Regina Bloomstine, Casimiro Ferreira, Andrii Pernatii, Kirill Hrymailo
+# BSD-3 License
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 # 1. Redistributions of source code must retain the above copyright notice,
