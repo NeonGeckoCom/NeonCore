22be909d6150d1f981e9d86443b8ab814ad73357
Daniel McKnight <daniel@neongecko.com>
2023-06-12 19:25:23 -0700

    More skill load troubleshooting

--- neon_core/skills/patched_plugin_loader.py
+++ neon_core/skills/patched_plugin_loader.py
@@ -5,12 +5,14 @@ from ovos_bus_client.message import Message
 from ovos_utils.log import LOG
 
 try:
+    from ovos_workshop.skill_launcher import SkillLoader
     from ovos_workshop.skill_launcher import PluginSkillLoader
     from ovos_workshop.skill_launcher import get_skill_class, \
         get_create_skill_function
 except ImportError:
     LOG.warning(f"Patching PluginSkillLoader")
     from mycroft.skills.skill_loader import PluginSkillLoader as _Plugin
+    from mycroft.skills.skill_loader import SkillLoader
     get_skill_class, get_create_skill_function = None, None
 
 
@@ -23,5 +25,6 @@ except ImportError:
             _Plugin.load(self, skill_class)
 
 
+mycroft.skills.skill_loader.SkillLoader = SkillLoader
 mycroft.skills.skill_loader.PluginSkillLoader = PluginSkillLoader
 mycroft.skills.skill_loader.Message = Message
