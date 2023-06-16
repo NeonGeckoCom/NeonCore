7fe18589429468ebc28cf2e00947111e28cbdd11
Daniel McKnight <daniel@neongecko.com>
2023-06-16 08:32:43 -0700

    Update logging in patched plugin skill loading Update skills to resolve load bugs

--- neon_core/skills/patched_plugin_loader.py
+++ neon_core/skills/patched_plugin_loader.py
@@ -15,9 +15,9 @@ try:
             skill_module = skill_module or self.skill_module
 
             try:
-                LOG.info(f"loading skill: {skill_module}")
                 # in skill classes __new__ should fully create the skill object
                 skill_class = self._skill_class or get_skill_class(skill_module)
+                LOG.info(f"loading skill: {skill_class}")
                 self.instance = skill_class(bus=self.bus, skill_id=self.skill_id)
                 if hasattr(self.instance, "initialize"):
                     self.instance.initialize()
