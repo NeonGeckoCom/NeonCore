672b7e52ec85087b34115aed7acb063b0bb04aab
Daniel McKnight <daniel@neon.ai>
2023-10-24 17:57:57 -0700

    Remove unused params in `neon_core.__init__` Mark init methods as deprecated Update skill installation to skip install methods when nothing to install Update dependencies

--- neon_core/config.py
+++ neon_core/config.py
@@ -32,15 +32,19 @@ def get_core_version() -> str:
     Get the core version string.
     NOTE: `init_config` should be called before this method
     """
-    # from neon_core.configuration import Configuration
-    # Configuration.load_all_configs({"disable_remote_config": True})
-
+    # TODO: Remove by 2024
+    from ovos_utils.log import log_deprecation
+    log_deprecation("This method is deprecated; use Python built-in utilities "
+                    "to find package versions", "23.12.1")
     # patch version string to allow downstream to know where it is running
-    import mycroft.version
-    core_version_str = '.'.join(map(str,
-                                    mycroft.version.CORE_VERSION_TUPLE)) + \
-                       "(NeonGecko)"
-    mycroft.version.CORE_VERSION_STR = core_version_str
+    try:
+        import mycroft.version
+        core_version_str = '.'.join(map(str,
+                                        mycroft.version.CORE_VERSION_TUPLE)) + \
+                           "(NeonGecko)"
+        mycroft.version.CORE_VERSION_STR = core_version_str
+    except (ImportError, AttributeError):
+        return ""
     return core_version_str
 
 
@@ -49,14 +53,20 @@ def setup_resolve_resource_file():
     Override default resolve_resource_file to include resources in neon-core.
     Priority: neon-utils, neon-core, ~/.local/share/neon, ~/.neon, mycroft-core
     """
+    from ovos_utils.log import log_deprecation
     from neon_utils.file_utils import resolve_neon_resource_file
-    from mycroft.util.file_utils import resolve_resource_file
+    log_deprecation("This method is deprecated; resource resolution is handled "
+                    "automatically.", "23.12.1")
 
     def patched_resolve_resource_file(res_name):
+        from mycroft.util.file_utils import resolve_resource_file
         resource = resolve_neon_resource_file(res_name) or \
-                   resolve_resource_file(res_name)
+            resolve_resource_file(res_name)
         return resource
 
-    import mycroft.util
-    mycroft.util.file_utils.resolve_resource_file = \
-        patched_resolve_resource_file
+    try:
+        import mycroft.util
+        mycroft.util.file_utils.resolve_resource_file = \
+            patched_resolve_resource_file
+    except (ImportError, AttributeError):
+        pass
