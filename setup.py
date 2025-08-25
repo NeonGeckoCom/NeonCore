9af05bd6a454d7fc3adf85bdc292b7688905bd77
Daniel McKnight <daniel@neon.ai>
2025-08-25 12:58:42 -0700

    Remove Deprecated methods and extra dependencies

--- setup.py
+++ setup.py
@@ -88,13 +88,6 @@ setup(
         "test": get_requirements("test.txt"),
         "pi": get_requirements("pi.txt"),
         "docker": get_requirements("docker.txt"),
-        # # TODO: Deprecate `_`-specified extras after 2024.5
-        "core_modules": get_requirements("core_modules.txt"),
-        "skills_required": get_requirements("skills_required.txt"),
-        "skills_essential": get_requirements("skills_essential.txt"),
-        "skills_default": get_requirements("skills_default.txt"),
-        "skills_extended": get_requirements("skills_extended.txt"),
-        # PEP503 specified normalization to `-`
         "core-modules": get_requirements("core_modules.txt"),
         "skills-required": get_requirements("skills_required.txt"),
         "skills-essential": get_requirements("skills_essential.txt"),
