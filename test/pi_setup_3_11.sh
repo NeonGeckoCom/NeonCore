7bc2483f32c39e8a6b181b35861c87cda77974d0
Daniel McKnight <daniel@neon.ai>
2025-11-06 08:24:49 -0800

    Don't cache Python packages in 3.11 test build to troubleshoot GHA faulre

--- test/pi_setup_3_11.sh
+++ test/pi_setup_3_11.sh
@@ -46,6 +46,6 @@ python3.11 -m venv "/core/venv" || exit 11
 pip install --upgrade pip wheel
 pip install https://github.com/smartgic/python-tflite-runtime/releases/download/2.13.0-cp311/tflite_runtime-2.13.0-cp311-cp311-linux_aarch64.whl
 #pip install https://whl.smartgic.io/tflite_runtime-2.13.0-cp311-cp311-linux_aarch64.whl
-pip install ".[core-modules,skills-required,skills-essential,skills-default,skills-extended,pi]" || exit 11
+pip install --no-cache-dir ".[core-modules,skills-required,skills-essential,skills-default,skills-extended,pi]" || exit 11
 
 cp -rf /core/test/pi_image_overlay/* /
